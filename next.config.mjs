0dbe95e6a34d7fd802dc50afb48bf15b64b3463f
Cab Sudo <69660242+Cab-sudo@users.noreply.github.com>
2026-04-24 07:02:34 -0700

    Add initial Next.js configuration

---
+++ next.config.mjs
@@ -0,0 +1,5 @@
+const nextConfig = {
+  experimental: { serverActions: { bodySizeLimit: '2mb' } },
+  images: { remotePatterns: [{ protocol: 'https', hostname: '**' }] },
+};
+export default nextConfig;
